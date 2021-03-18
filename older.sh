9703cc0ad41ddc1cffc985db020973c0dbe87dde
MustansirAbbas <80603560+MustansirAbbas@users.noreply.github.com>
2021-03-19 00:33:24 +0100

    Update older.sh

--- older.sh
+++ older.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#checking if atleast one arguement is passed
 set -e
 if [ "$#" -lt 1 ]
 then
@@ -9,14 +10,21 @@ else
 echo -e "\c"
 fi
 
+# initializing with oldest with first filename
+
 oldest=$1
 
+# using loops
 for file in "$@"
 do
+
+ #each time checking if the other files are older than the one initalied 
  if [[ $file -ot $oldest ]]
  then
-	oldest=$file
+ #swapping if the new file is older then previous
+  oldest=$file
  fi
 done
 
+# return the oldest file
 echo "$oldest"
